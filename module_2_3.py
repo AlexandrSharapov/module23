86cd55c1a52365947ff232f9025419abf1b220e7
Александр <crb.mail@yandex.ru>
2024-11-10 12:12:47 +0500

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -4,7 +4,7 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0 #начальное значение индекса
 while index < len(my_list): #индекс меньше длины списка
     if my_list[index] < 0: 
-        index += 1 
+        index += 1 #используется след. индекс
         continue
     print(my_list[index]) 
     index += 1 
